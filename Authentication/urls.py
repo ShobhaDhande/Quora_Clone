30eb85d4fa7dc97fb4da32a3e6d80fa27098a3d8
shobha11552 <shobhadhande11@gmail.com>
2025-04-09 21:05:07 +0530

    Quora clone code

--- Authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import LoginView,SignupView,LogOutView
-
-
-urlpatterns = [
-    path('', LoginView.as_view(),name='login'),
-    path('signup/',SignupView.as_view(), name='signup'),
-    path('logout/',LogOutView.as_view(), name='logout')
-]
--- Dashboard/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import QuestionModel,AnswerModel,LikeModel
-# Register your models here.
-
-admin.site.register(QuestionModel)
-admin.site.register(AnswerModel)
-admin.site.register(LikeModel)
